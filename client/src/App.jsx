c824336a0ed34e2ac28f160b96a745c26491b3d7
Dragon <parit.menklay@gmail.com>
2026-01-13 16:13:52 +0700

    feat: integrate axios for product data fetching and deletion in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,56 @@
 import "./App.css";
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 function App() {
+  const [products, setProduct] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.get("http://localhost:4001/products");
+    setProduct(response.data.data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const deleteProduct = async (id) => {
+    await axios.delete(`http://localhost:4001/products/${id}`);
+    getData();
+  };
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+        {products.map((product) => {
+          return (
+            <div className="product" key={product.id}>
+              <div className="product-preview">
+                <img
+                  src={product.image}
+                  alt="some product"
+                  width="350"
+                  height="350"
+                />
+              </div>
+              <div className="product-detail">
+                <h1>Product name: {product.name}</h1>
+                <h2>Product price: {product.price} Baht</h2>
+                <p>Product description: {product.description}</p>
+              </div>
 
-          <button className="delete-button">x</button>
-        </div>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  deleteProduct(product.id);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
